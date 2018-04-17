356a34bc62fdf6055d1bb63fb40eaa475bae9f49
Roniece A Ricardo <roniecericardo@gmail.com>
2018-04-17 20:06:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,15 +13,3 @@ function nowServing(katzDeliLine){
   }
 }
 
-function currentLine(katzDeliLine){
-  if (katzDeliLine.length === 0){
-    return 'The line is currently empty.';
-  } else {
-    var currentLine = 'The line is currently:'
-    currentLine + katzDeliLine.reduce(function (customer, position){
-      position = 0;
-      currentLine =+ (position + 1) + '. ' + (customer)
-      position++;
-    }.join(','))
-    return currentLine;
-}
